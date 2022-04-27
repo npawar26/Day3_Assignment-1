5bf704174a39e363c7ccbd7c4438b9da4bb4cfc2
npawar26 <102570252+npawar26@users.noreply.github.com>
2022-04-27 12:35:53 +0530

    Update PartTimeEmpWage.java

--- PartTimeEmpWage.java
+++ PartTimeEmpWage.java
@@ -10,13 +10,13 @@ public class PartTimeEmpWage {
 		int empWage = 0;
 
 		double empCheck = Math.floor(Math.random() * 10) % 3;
-		if empCheck == IS_FULL_TIME)
+		if (empCheck == IS_FULL_TIME)
 			empHrs = 8;
-		else if empCheck == IS_PART_TIME)
+		else if (empCheck == IS_PART_TIME)
 			empHrs = 4;
 		else
 			empHrs = 0;
-		empwage = empHrs * EMP_RATE_PER_HOUR 
+		empWage = empHrs * EMP_RATE_PER_HOUR 
 		System.out.println("Employee wage : " + empWage);	
 
 	}
